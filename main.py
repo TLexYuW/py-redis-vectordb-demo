5d2fca3cbadecf6b266964901352e7e474583c22
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-11 22:27:31 +0800

    update

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ import os
 # redis-py ---------------------------------------------------------------------------------------------------------------
 
 # Pre-train Model
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-r = Redis(host='localhost', port=6379, db=0)
+r = Redis(host="localhost", port=6379, db=0)
 
 # text = "Understanding vector search is easy, but understanding all the mathematics behind a vector is not!"
 # embedding = model.encode(text)
@@ -23,7 +23,7 @@ r = Redis(host='localhost', port=6379, db=0)
 # r = Redis(host='localhost', port=6379, db=0)
 # r.set('foo', 'bar')
 # print(r.get('foo'))
-      
+
 # blob = embedding.astype(np.float32).tobytes()
 # r.hset('doc:1', mapping = {'embedding': blob,
 #                            'genre': 'technical',
@@ -37,18 +37,48 @@ r = Redis(host='localhost', port=6379, db=0)
 # }
 # r.json().set("doc:2", '$', doc)
 
+# ------------------------------------------------------------------------------------
 
 # Create the index
 # index_def = IndexDefinition(prefix=["doc:"])
-# schema = (  TextField("content", as_name="content"),
-#             TagField("genre", as_name="genre"),
-#             VectorField("embedding", "FLAT", {"TYPE": "FLOAT32", "DIM": 384, "DISTANCE_METRIC": "COSINE"}))
-# r.ft('vec_idx').create_index(schema, definition=index_def)
+# schema = (
+#     TextField("content", as_name="content"),
+#     TagField("genre", as_name="genre"),
+#     VectorField(
+#         "embedding",
+#         "FLAT",
+#         {"TYPE": "FLOAT32", "DIM": 384, "DISTANCE_METRIC": "COSINE"},
+#     ),
+# )
+# r.ft("vec_idx").create_index(schema, definition=index_def)
 
 # Import sample data
-# r.hset('doc:1', mapping = {'embedding': model.encode("That is a very happy person").astype(np.float32).tobytes(), 'genre': 'persons', 'content': "That is a very happy person"})
-# r.hset('doc:2', mapping = {'embedding': model.encode("That is a happy dog").astype(np.float32).tobytes(), 'genre': 'pets', 'content': "That is a happy dog"})
-# r.hset('doc:3', mapping = {'embedding': model.encode("Today is a sunny day").astype(np.float32).tobytes(), 'genre': 'weather', 'content': "Today is a sunny day"})
+# r.hset(
+#     "doc:1",
+#     mapping={
+#         "embedding": model.encode("That is a very happy person")
+#         .astype(np.float32)
+#         .tobytes(),
+#         "genre": "persons",
+#         "content": "That is a very happy person",
+#     },
+# )
+# r.hset(
+#     "doc:2",
+#     mapping={
+#         "embedding": model.encode("That is a happy dog").astype(np.float32).tobytes(),
+#         "genre": "pets",
+#         "content": "That is a happy dog",
+#     },
+# )
+# r.hset(
+#     "doc:3",
+#     mapping={
+#         "embedding": model.encode("Today is a sunny day").astype(np.float32).tobytes(),
+#         "genre": "weather",
+#         "content": "Today is a sunny day",
+#     },
+# )
 
 # This is the test sentence
 sentence = "That is a happy person"
@@ -59,25 +89,42 @@ sentence = "That is a happy person"
 # sentence = "human"
 # sentence = "child"
 
-# q = Query("*=>[KNN 2 @embedding $vec AS score]").sort_by("score", asc=True).return_field("score").return_field("content").dialect(2)
-# res = r.ft("vector_idx").search(q, query_params={"vec": model.encode(sentence).astype(np.float32).tobytes()})
+# ------------------------------------------------------------------------------------
 
-radius = 0.2
-vec = model.encode(sentence).astype(np.float32).tobytes()
-q = Query("@embedding:[VECTOR_RANGE $radius $vec]=>{$YIELD_DISTANCE_AS: score}") \
-    .return_fields("score", "content") \
+q = (
+    Query("*=>[KNN 2 @embedding $vec AS score]")
+    .sort_by("score", asc=True)
+    .return_field("score")
+    .return_field("content")
     .dialect(2)
+)
+res = r.ft("vec_idx").search(
+    q, query_params={"vec": model.encode(sentence).astype(np.float32).tobytes()}
+)
 
-query_params = {
-    "radius": radius,
-    "vec": vec
-}
+# ------------------------------------------------------------------------------------
 
-res = r.ft("vec_idx").search(query=q, query_params=query_params)
+# radius = 0.2
+# vec = model.encode(sentence).astype(np.float32).tobytes()
+# q = (
+#     Query("@embedding:[VECTOR_RANGE $radius $vec]=>{$YIELD_DISTANCE_AS: score}")
+#     .return_fields("score", "content")
+#     .dialect(2)
+# )
+# query_params = {"radius": radius, "vec": vec}
+# res = r.ft("vec_idx").search(query=q, query_params=query_params)
 
-# q = Query("@genre:{pets}=>[KNN 2 @embedding $vec AS score]").return_field("score").return_field("content").dialect(2)
-# res = r.ft("vector_idx").search(q, query_params={"vec": model.encode(sentence).astype(np.float32).tobytes()})
+# ------------------------------------------------------------------------------------
 
+# q = (
+#     Query("@genre:{pets}=>[KNN 2 @embedding $vec AS score]")
+#     .return_field("score")
+#     .return_field("content")
+#     .dialect(2)
+# )
+# res = r.ft("vec_idx").search(
+#     q, query_params={"vec": model.encode(sentence).astype(np.float32).tobytes()}
+# )
 
 print(res)
 
